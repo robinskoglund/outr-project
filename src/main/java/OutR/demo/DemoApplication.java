7d91319c654be090943ea1b0822851380666109f
johantors <stroeum@gmail.com>
2022-04-06 14:49:13 +0200

    egg

--- src/main/java/OutR/demo/DemoApplication.java
+++ src/main/java/OutR/demo/DemoApplication.java
@@ -16,6 +16,6 @@ public class DemoApplication {
 
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World Hehe") String name) {
-		return String.format("Hello %s!", name);
+		return String.format("Hello %s!", name + " varför jobbar du inte?");
 	}
 }
