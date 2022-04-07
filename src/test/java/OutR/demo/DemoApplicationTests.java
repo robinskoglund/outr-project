8f2fc015d68ac94f368af872870661353dde9cb4
robinskoglund <robins_92@hotmail.com>
2022-04-07 15:12:46 +0200

    updated to java 8

--- src/test/java/OutR/demo/DemoApplicationTests.java
+++ src/test/java/OutR/demo/DemoApplicationTests.java
@@ -1,7 +1,5 @@
 package OutR.demo;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,14 +8,6 @@ class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
-	}
-
-	/**
-	 * Added just to maintain the proper import for asserttions, "import static org.junit.jupiter.api.Assertions.*;"
-	 */
-	@Test
-	void test() {
-		assertTrue(true);
 	}
 
 }
