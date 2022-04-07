8020c476b838b2664b7f6d13857b555537ea04b6
johantors <stroeum@gmail.com>
2022-04-07 11:38:24 +0200

    pom.xml and DemoApplication should be prepared for Tomcat. Not 100% on distributionManagement.

--- src/main/java/OutR/demo/DemoApplication.java
+++ src/main/java/OutR/demo/DemoApplication.java
@@ -2,13 +2,14 @@ package OutR.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
