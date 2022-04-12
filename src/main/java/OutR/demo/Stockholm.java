09861bb49c1ef70854a220eb1a093db704bf73b2
johantors <stroeum@gmail.com>
2022-04-12 17:53:40 +0200

    Stockholm.convertCoordinates now converts coordinates from Stockholms City's utegym API to be used in Google Maps.
    Added connected test and library.

--- src/main/java/OutR/demo/Stockholm.java
+++ src/main/java/OutR/demo/Stockholm.java
@@ -1,5 +1,28 @@
 package OutR.demo;
 
+import com.github.goober.coordinatetransformation.positions.SWEREF99Position;
+import com.github.goober.coordinatetransformation.positions.WGS84Position;
+
+
 public class Stockholm {
 
+    /**
+     * Takes coordinates as written in Stockholms stads API (utegym at least) and returns a String that is searchable
+     * in google maps API where Lat and Long is separated by a comma.
+     * @author Johan
+     * @param north N coordinate in SWEREF99 18 00
+     * @param east N coordinate in SWEREF99 18 00
+     * @return latitude, longitude in decimal coordinates
+     */
+    public String convertCoordinates(int north, int east) {
+        SWEREF99Position sweref99Position = new SWEREF99Position(north, east,
+                SWEREF99Position.SWEREFProjection.sweref_99_18_00);
+        sweref99Position.toWGS84();
+
+        WGS84Position wgs84 = sweref99Position.toWGS84();
+        double latitude = wgs84.getLatitude();
+        double longitude = wgs84.getLongitude();
+
+        return latitude + ", " + longitude;
+    }
 }
--- src/test/java/OutR/demo/DemoApplicationTests.java
+++ src/test/java/OutR/demo/DemoApplicationTests.java
@@ -1,5 +1,8 @@
 package OutR.demo;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +11,31 @@ class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void coordinates() {
+		// Two pairs of outdoor gym coordinates from Stockholms Utegym-API
+		final int testValueN = 6576875;
+		final int testValueE = 151468;
+		final int vasaParkenN = 6580442;
+		final int vasaParkenE = 152307;
+
+		Stockholm stockholm = new Stockholm();
+		Assertions.assertEquals("59.307058459354174, 18.025771228832983", stockholm.convertCoordinates(testValueN, testValueE));
+		Assertions.assertEquals("59.33907425152833, 18.04053825473325", stockholm.convertCoordinates(vasaParkenN, vasaParkenE));
+
+		// Since google maps coordinates are rounded, comparison is made with one less decimal.
+		// The actual difference is minimal on the map
+		MatcherAssert.assertThat(stockholm.convertCoordinates(vasaParkenN, vasaParkenE), CoreMatchers.allOf(
+				CoreMatchers.containsString("59.33907"),
+				CoreMatchers.containsString("18.04053")
+		));
+		MatcherAssert.assertThat(stockholm.convertCoordinates(testValueN, testValueE), CoreMatchers.allOf(
+				CoreMatchers.containsString("59.30705"),
+				CoreMatchers.containsString("18.02577")
+		));
+
 	}
 
 }
