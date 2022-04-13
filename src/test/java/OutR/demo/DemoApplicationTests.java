7ee0474d4a1ab69b60b8aa6fba3f18935ac0b9f7
johantors <stroeum@gmail.com>
2022-04-13 10:26:50 +0200

    convertCoordinates returns double[], not String

--- src/test/java/OutR/demo/DemoApplicationTests.java
+++ src/test/java/OutR/demo/DemoApplicationTests.java
@@ -26,19 +26,17 @@ class DemoApplicationTests {
         final int vasaParkenE = 152307;
 
         Stockholm stockholm = new Stockholm();
-        Assertions.assertEquals("59.307058459354174, 18.025771228832983", stockholm.convertCoordinates(testValueN, testValueE));
-        Assertions.assertEquals("59.33907425152833, 18.04053825473325", stockholm.convertCoordinates(vasaParkenN, vasaParkenE));
 
         // Since google maps coordinates are rounded, comparison is made with one less decimal.
         // The actual difference is minimal on the map
-//        MatcherAssert.assertThat(Arrays.toString(stockholm.convertCoordinates(vasaParkenN, vasaParkenE)), CoreMatchers.allOf(
-//                CoreMatchers.containsString("59.33907"),
-//                CoreMatchers.containsString("18.04053")
-//        ));
-//        MatcherAssert.assertThat(Arrays.toString(stockholm.convertCoordinates(testValueN, testValueE)), CoreMatchers.allOf(
-//                CoreMatchers.containsString("59.30705"),
-//                CoreMatchers.containsString("18.02577")
-//        ));
+        MatcherAssert.assertThat(Arrays.toString(stockholm.convertCoordinates(vasaParkenN, vasaParkenE)), CoreMatchers.allOf(
+                CoreMatchers.containsString("59.33907"),
+                CoreMatchers.containsString("18.04053")
+        ));
+        MatcherAssert.assertThat(Arrays.toString(stockholm.convertCoordinates(testValueN, testValueE)), CoreMatchers.allOf(
+                CoreMatchers.containsString("59.30705"),
+                CoreMatchers.containsString("18.02577")
+        ));
 
     }
 
