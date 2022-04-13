143eee045a6892afa2963ea90466a0f7feb618ad
johantors <stroeum@gmail.com>
2022-04-13 14:49:09 +0200

    APIHandler added unpackOutdoorGym creates array of gyms from API New constructor with Name for OutdoorGym Removed id from Node params

--- src/main/java/OutR/demo/Node.java
+++ src/main/java/OutR/demo/Node.java
@@ -7,10 +7,10 @@ public class Node {
     private final Double latitude;
     private final Integer id;
 
-    public Node(Double longitude, Double latitude, Integer id){
+    public Node(Double longitude, Double latitude){
         this.longitude=longitude;
         this.latitude=latitude;
-        this.id=id;
+        id = 1; //TODO: Fix
     }
     public Double getLongitude(){return longitude;}
     public Double getLatitude(){return latitude;}
