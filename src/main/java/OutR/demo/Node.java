a10e640cf82dd33b638510ab7466d1c90af1a199
johantors <stroeum@gmail.com>
2022-04-14 10:37:09 +0200

    Removed ID from Node-class

--- src/main/java/OutR/demo/Node.java
+++ src/main/java/OutR/demo/Node.java
@@ -1,20 +1,16 @@
 package OutR.demo;
 
-import java.util.ArrayList;
-
 public class Node {
     private final Double  longitude;
     private final Double latitude;
-    private final Integer id;
 
     public Node(Double longitude, Double latitude){
         this.longitude=longitude;
         this.latitude=latitude;
-        id = 1; //TODO: Fix
     }
+
     public Double getLongitude(){return longitude;}
     public Double getLatitude(){return latitude;}
-    public Integer getId() {return id;}
     public String toString() {
         return "Longitude: " +longitude + " Latitude: " + latitude;
     }
