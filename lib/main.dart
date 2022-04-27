f54ec4920be577d74338ccc6f9c81147d73caa22
robinskoglund <robins_92@hotmail.com>
2022-04-28 00:43:45 +0200

    stretch backround for map view page

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.green[900],
       ),
       body: Container(
+        width: MediaQuery.of(context).size.width,
         decoration: const BoxDecoration(
           image: DecorationImage(
             image: AssetImage('assets/greenbackgroundtest.jpg'),
@@ -61,16 +62,40 @@ class Home extends StatelessWidget {
 class MapViewTest extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Map View')
+        title: const Text('Outr Demo'),
+        centerTitle: true,
+        backgroundColor: Colors.green[900],
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('Go back!'),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/greenbackgroundtest.jpg'),
+              fit: BoxFit.cover,
+            )
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 250.0, horizontal: 135.0),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                ElevatedButton.icon(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.green,
+                  ),
+                    icon: const Icon(
+                    Icons.arrow_back
+                  ),
+                  label: const Text('Back')
+                ),
+              ]
+          ),
         ),
       ),
     );
