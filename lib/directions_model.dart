e902b17e6358b68dbc6258b702d37c76f8bc4c7c
robinskoglund <robins_92@hotmail.com>
2022-05-05 13:36:17 +0200

    Removed bounds from Directions, can now visualize JSON directions on map.

--- lib/directions_model.dart
+++ lib/directions_model.dart
@@ -3,13 +3,11 @@ import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Directions {
-  final LatLngBounds bounds;
   final List<PointLatLng> polylinePoints;
   final String totalDistance;
   final String totalDuration;
 
   const Directions({
-    required this.bounds,
     required this.polylinePoints,
     required this.totalDistance,
     required this.totalDuration,
@@ -23,12 +21,7 @@ class Directions {
     final data = Map<String, dynamic>.from(map['routes'][0]);
 
     //Bounds, makes sure the camera fits all the polylinepoints
-    final northeast = data['bounds']['northeast'];
-    final southwest = data['bounds']['southwest'];
-    final bounds = LatLngBounds(
-      southwest: LatLng(northeast['lat'], northeast['lng']),
-      northeast: LatLng(southwest['lat'], southwest['lng']),
-    );
+
 
     // Distances and duration
     String distance = '';
@@ -40,7 +33,6 @@ class Directions {
     }
 
     return Directions(
-      bounds: bounds,
       polylinePoints: PolylinePoints().decodePolyline(data['overview_polyline']['points']),
       totalDistance: distance,
       totalDuration: duration,
