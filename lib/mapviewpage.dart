947e50ce2ac38199177d2bf8026e5906b4760386
robinskoglund <robins_92@hotmail.com>
2022-05-12 13:59:49 +0200

    Markers for gym and geoLocation added. Moved geoposition stuff to mapviewpage since this is where we will use the geolocation anyways. Added marker on geolocation and how to handle the gym info as a marker aswell

--- lib/mapviewpage.dart
+++ lib/mapviewpage.dart
@@ -1,10 +1,14 @@
+import 'dart:collection';
+
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
 import 'directions_model.dart';
 import 'geopositionpage.dart';
+import 'package:geolocator/geolocator.dart';
 import 'httprequesthandler.dart';
 
 /*
@@ -18,20 +22,32 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
-  GoogleMapController? _controller;
 
-  final CameraPosition _initialCameraPosition = const CameraPosition(
-    target: LatLng(59.330668, 18.056498),
-    zoom: 11.5,
-  );
-
+  final CameraPosition _initialCameraPosition = const CameraPosition(target: LatLng(59.330668, 18.056498), zoom: 11.5,);
   late GoogleMapController _googleMapController;
   Directions? _info;
+  late Position geoPosition;
+  Set<Marker> _markers = HashSet<Marker>();
+  late BitmapDescriptor _markerIcon;
+  String gymName = '';
+  double gymLat = 0;
+  double gymLong = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentLocation();
+    _setGymMarkerIcon();
+  }
 
   @override
   void dispose() {
     _googleMapController.dispose();
     super.dispose();
+  }
+
+  void _setGymMarkerIcon() async {
+    _markerIcon = await BitmapDescriptor.fromAssetImage(ImageConfiguration(), 'assets/barbell.png');
   }
 
   @override
@@ -48,7 +64,7 @@ class _MapScreenState extends State<MapScreen> {
         zoomControlsEnabled: false,
         initialCameraPosition: _initialCameraPosition,
         onMapCreated: (controller) => _googleMapController = controller,
-        //markers: {}
+        markers: _markers,
         polylines: {
           if (_info != null)
             Polyline(
@@ -62,17 +78,94 @@ class _MapScreenState extends State<MapScreen> {
         },
       ),
       floatingActionButton: FloatingActionButton(onPressed: () {
-        _populateInfo();
+        //_populateInfo();
+        testPopulate();
+        _setGymMarkerIcon();
       },
       ),
 
     );
   }
 
-
   void _populateInfo() async {
     final directions = await HttpRequestHandler()
         .getDirections();
-    setState(() => _info = directions);
+    setState(() {
+      _info = directions;
+      _markers.add(
+        Marker(
+          markerId: MarkerId("0"),
+          position: LatLng(geoPosition.latitude, geoPosition.longitude),
+          infoWindow: InfoWindow(
+            title: 'My Position',
+            snippet: 'My current position',
+          ),
+        ),
+      );
+
+      //Gym marker, switch lat and longs for gymLat and gymLong
+      _markers.add(
+        Marker(
+        markerId: MarkerId("1"),
+        position: LatLng(37.421817, -122.083691),
+        infoWindow: InfoWindow(
+          title: gymName,
+          snippet: 'Gym location of' + gymName,
+        ),
+          icon: _markerIcon,
+      ),
+      );
+    });
   }
+
+  //Get location for showing geo location marker on map
+  getCurrentLocation() async {
+    var locatePosition = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+
+    setState(() {
+      geoPosition = locatePosition;
+    });
+  }
+
+  //update the gym information based on the get request from pathfinder, after the "-"
+  void setGymInformation() {
+    String temp = HttpRequestHandler().getStringFromPathfinder(false) as String;
+    temp.split('|');
+    setState(() {
+      gymName = temp[0];
+      gymLat = double.parse(temp[1]);
+      gymLong = double.parse(temp[2]);
+    });
+  }
+
+  //Temporary test marker method, since the get request from pathfinder isnt fully functional yet
+  void testPopulate() {
+    setState(() {
+      _markers.add(
+        Marker(
+          markerId: MarkerId("0"),
+          position: LatLng(geoPosition.latitude, geoPosition.longitude),
+          infoWindow: InfoWindow(
+            title: 'My Position',
+            snippet: 'My current position',
+          ),
+        ),
+      );
+
+      //Gym marker, switch lat and longs for gymLat and gymLong
+      _markers.add(
+        Marker(
+          markerId: MarkerId("1"),
+          position: LatLng(37.421817, -122.083691),
+          infoWindow: InfoWindow(
+            title: gymName,
+            snippet: 'Gym location of' + gymName,
+          ),
+          icon: _markerIcon,
+        ),
+      );
+    });
+  }
+
 }
