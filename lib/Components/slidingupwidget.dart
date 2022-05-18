ccdc7d2172abcacfdce794dd7081eb5eb6a9c7f8
johantors <stroeum@gmail.com>
2022-05-18 20:47:30 +0200

    slidingupwidget got a Fake active UI

--- lib/Components/slidingupwidget.dart
+++ lib/Components/slidingupwidget.dart
@@ -3,158 +3,279 @@ import 'package:flutter/material.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 import '../ScreenPages/mapviewpage.dart';
 
-class SlidingUpWidget extends StatelessWidget{
+class SlidingUpWidget extends StatefulWidget {
   final PanelController panelController;
   final ButtonChoiceCallback chooseButton;
 
-
-  const SlidingUpWidget(
-  {Key? key,
-    required this.panelController, required this.chooseButton,
+  const SlidingUpWidget({
+    Key? key,
+    required this.panelController,
+    required this.chooseButton,
   }) : super(key: key);
 
   @override
+  State<SlidingUpWidget> createState() => _SlidingUpWidgetState();
+}
+
+class _SlidingUpWidgetState extends State<SlidingUpWidget> {
+  bool _isActive = false;
+  bool _isPaused = false;
+  String _playPause = "Pause";
+  String _sliderHeader = 'Start';
+
+  //TODO: Testa att lägga Center utanför som en stateful?
+  //TODO: Testa att
+
+  @override
   Widget build(BuildContext context) => ListView(
-    padding: EdgeInsets.zero,
-    children: <Widget>[
-      const SizedBox(height: 15),
-      openUpPanelDragHandle(),
-
-      Center(
-        child: ElevatedButton(
-          onPressed: () {togglePanelUpDown();},
-          style: ButtonStyle(
-            minimumSize: MaterialStateProperty.all(Size(390, 30)),
-            backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
-            elevation: MaterialStateProperty.all<double>(0),
-          ), child: const Text(
-          'Start',
-          style: TextStyle(
-            fontSize: 60.0,
-            letterSpacing: 1.5,
-            color: Colors.blueGrey,
-            fontFamily: 'Dongle',
-          ),
-        ),
-        ),
-      ),
-
-      Padding(
-        padding: EdgeInsets.fromLTRB(20, 0, 20, 20),
-        child: SizedBox(
-          height: 80,
-          child: Directionality(
-            textDirection: TextDirection.ltr,
-            child: FloatingActionButton.extended(
-              heroTag: 'beginnerbutton',
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20.0))
-              ),
-              backgroundColor: Colors.white,
-              foregroundColor: Colors.black,
-              onPressed: (){
-                chooseButton(1);
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          const SizedBox(height: 15),
+          openUpPanelDragHandle(),
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                togglePanelUpDown();
               },
-              icon: Image.asset('assets/plainDude.png'),
-              label: Text('Beginner Program'),
-              extendedTextStyle: TextStyle(fontFamily: 'Dongle', fontSize: 50),
+              style: ButtonStyle(
+                minimumSize: MaterialStateProperty.all(Size(390, 30)),
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                elevation: MaterialStateProperty.all<double>(0),
+              ),
+              child: Text(
+                _sliderHeader,
+                style: TextStyle(
+                  fontSize: 60.0,
+                  letterSpacing: 1.5,
+                  color: Colors.blueGrey,
+                  fontFamily: 'Dongle',
+                ),
+              ),
             ),
           ),
-        ),
-      ),
-
-      Padding(
-        padding: EdgeInsets.fromLTRB(20, 0, 20, 20),
-        child: SizedBox(
-          height: 80,
-          child: Directionality(
-            textDirection: TextDirection.rtl,
-            child: FloatingActionButton.extended(
-              heroTag: 'cardiobutton',
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20.0))
-              ),
-              backgroundColor: Colors.white,
-              foregroundColor: Colors.black,
-              onPressed: (){
-                chooseButton(2);
-              },
-              icon: Image.asset('assets/cardioDude.png'),
-              label: Text('Cardio'),
-              extendedTextStyle: TextStyle(fontFamily: 'Dongle', fontSize: 50),
+          Visibility(
+            visible: !_isActive,
+            child: Stack(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.fromLTRB(40, 0, 40, 40),
+                  child: Container(
+                    width: 400,
+                    height: 400,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        FloatingActionButton.extended(
+                          heroTag: 'beginnerbutton',
+                          shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(20.0))),
+                          backgroundColor: Colors.white,
+                          foregroundColor: Colors.black,
+                          onPressed: () {
+                            togglePanelUpDown();
+                            // widget.chooseButton(1);
+                            changeState();
+                          },
+                          icon: Image.asset('assets/plainDude.png'),
+                          label: Text('Beginner Program'),
+                          extendedTextStyle:
+                              TextStyle(fontFamily: 'Dongle', fontSize: 50),
+                        ),
+                        FloatingActionButton.extended(
+                          heroTag: 'cardiobutton',
+                          shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(20.0))),
+                          backgroundColor: Colors.white,
+                          foregroundColor: Colors.black,
+                          onPressed: () {
+                            togglePanelUpDown();
+                            // widget.chooseButton(2);
+                            changeState();
+                          },
+                          icon: Image.asset('assets/cardioDude.png'),
+                          label: Text('Cardio'),
+                          extendedTextStyle:
+                              TextStyle(fontFamily: 'Dongle', fontSize: 50),
+                        ),
+                        FloatingActionButton.extended(
+                          heroTag: 'strengthbutton',
+                          shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(20.0))),
+                          backgroundColor: Colors.white,
+                          foregroundColor: Colors.black,
+                          onPressed: () {
+                            togglePanelUpDown();
+                            // widget.chooseButton(3);
+                            changeState();
+                          },
+                          icon: Image.asset('assets/strengthDude.png'),
+                          label: Text('Strength'),
+                          extendedTextStyle:
+                              TextStyle(fontFamily: 'Dongle', fontSize: 50),
+                        ),
+                        FloatingActionButton.extended(
+                          heroTag: 'mixbutton',
+                          shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(20.0))),
+                          backgroundColor: Colors.white,
+                          foregroundColor: Colors.black,
+                          onPressed: () {
+                            togglePanelUpDown();
+                            // widget.chooseButton(4);
+                            changeState();
+                          },
+                          icon: Image.asset('assets/mixDude.png'),
+                          label: Text('Mix'),
+                          extendedTextStyle:
+                              TextStyle(fontFamily: 'Dongle', fontSize: 50),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
-        ),
-      ),
-
-      Padding(
-        padding: EdgeInsets.fromLTRB(20, 0, 20, 20),
-        child: SizedBox(
-          height: 80,
-          child: Directionality(
-            textDirection: TextDirection.rtl,
-            child: FloatingActionButton.extended(
-              heroTag: 'strengthbutton',
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20.0))
-              ),
-              backgroundColor: Colors.white,
-              foregroundColor: Colors.black,
-              onPressed: (){
-                chooseButton(3);
-              },
-              icon: Image.asset('assets/strengthDude.png'),
-              label: Text('Strength'),
-              extendedTextStyle: TextStyle(fontFamily: 'Dongle', fontSize: 50),
+          Visibility(
+            visible: _isActive,
+            child: Stack(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.fromLTRB(40, 0, 40, 40),
+                  child: Container(
+                    width: 400,
+                    height: 400,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Column(
+                          children: <Widget>[
+                            Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: <Widget>[
+                                  Text(
+                                    "0,60",
+                                    style: TextStyle(
+                                      fontSize: 60.0,
+                                      letterSpacing: 1.5,
+                                      color: Colors.black,
+                                      fontFamily: 'Dongle',
+                                    ),
+                                  ),
+                                  Text("12,47",
+                                      style: TextStyle(
+                                        fontSize: 60.0,
+                                        letterSpacing: 1.5,
+                                        color: Colors.black,
+                                        fontFamily: 'Dongle',
+                                      )),
+                                ]),
+                            Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: <Widget>[
+                                  Text("KM left",
+                                      style: TextStyle(
+                                        fontSize: 40.0,
+                                        letterSpacing: 1.5,
+                                        color: Colors.black,
+                                        fontFamily: 'Dongle',
+                                      )),
+                                  Text("Min/KM",
+                                      style: TextStyle(
+                                        fontSize: 40.0,
+                                        letterSpacing: 1.5,
+                                        color: Colors.black,
+                                        fontFamily: 'Dongle',
+                                      ))
+                                ]),
+                          ],
+                        ),
+                        FloatingActionButton.extended(
+                          shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(20.0))),
+                          backgroundColor: Colors.white,
+                          foregroundColor: Colors.black,
+                          onPressed: () {
+                            togglePanelUpDown();
+                            setState(() {
+                              if (_playPause == 'Pause') {
+                                _playPause = 'Play';
+                              } else {
+                                _playPause = "Pause";
+                              }
+                              _isPaused = !_isPaused;
+                            });
+                          },
+                          icon: Icon((_isPaused == false) ? Icons.pause_circle : Icons.play_arrow, size: 40),
+                          label: Text(_playPause),
+                          extendedTextStyle:
+                              TextStyle(fontFamily: 'Dongle', fontSize: 50),
+                        ),
+                        FloatingActionButton.extended(
+                          shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(20.0))),
+                          backgroundColor: Colors.red,
+                          foregroundColor: Colors.black,
+                          onPressed: () {
+                            changeState();
+                            togglePanelUpDown();
+                          },
+                          label: Text('End'),
+                          extendedTextStyle: TextStyle(
+                              fontFamily: 'Dongle',
+                              fontSize: 50,
+                              color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
-        ),
-      ),
-
-      Padding(
-        padding: EdgeInsets.fromLTRB(20, 0, 20, 20),
-        child: SizedBox(
-          height: 80,
-          child: Directionality(
-            textDirection: TextDirection.rtl,
-            child: FloatingActionButton.extended(
-              heroTag: 'mixbutton',
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20.0))
-              ),
-              backgroundColor: Colors.white,
-              foregroundColor: Colors.black,
-              onPressed: (){
-                chooseButton(4);
-              },
-              icon: Image.asset('assets/mixDude.png'),
-              label: Text('Mix'),
-              extendedTextStyle: TextStyle(fontFamily: 'Dongle', fontSize: 50),
-            ),
-          ),
-        ),
-      ),
-
-    ],
-  );
+        ],
+      );
 
   Widget openUpPanelDragHandle() => GestureDetector(
-    child: Center(
-      child: Container(
-        width: 50,
-        height: 5,
-        decoration: BoxDecoration(
-          color: Colors.grey[600],
-          borderRadius: BorderRadius.circular(12),
+        child: Center(
+          child: Container(
+            width: 50,
+            height: 5,
+            decoration: BoxDecoration(
+              color: Colors.grey[600],
+              borderRadius: BorderRadius.circular(12),
+            ),
+          ),
         ),
-      ),
-    ),
-    onTap: togglePanelUpDown,
-  );
+        onTap: togglePanelUpDown,
+      );
+
+  void changeState() {
+    setState(() {
+      if (!_isActive) {
+        _isActive = true;
+        _sliderHeader = '31:00' + '   ' + '2,02' + '    ' + '7:00';
+      } else {
+        _isActive = false;
+        _sliderHeader = 'Start';
+      }
+    });
+  }
 
   //Den här skiten funkar ju bara när den är nere
-  void togglePanelUpDown() => panelController.isPanelOpen ? panelController.close() : panelController.open();
-
+  void togglePanelUpDown() => widget.panelController.isPanelOpen
+      ? widget.panelController.close()
+      : widget.panelController.open();
 }
 
 typedef ButtonChoiceCallback = void Function(int buttonNumber);
