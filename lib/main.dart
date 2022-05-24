0b97252624ece37b750504288e0c185dba0ec807
erikankarstad <Rillerulle2>
2022-05-24 10:20:37 +0200

    App can now start on iOS. Added main.dart as a starting point for iOS and chenged flutter_polylines to from 0.2.6 til 1.0.0

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:outr/ScreenPages/login_view_page.dart';
+
+void main() => runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: new Home()),
+    );
